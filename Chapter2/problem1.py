5d6da383dd4802e1d9e40aafd40d4b30780e766f
honneer <131390915+honneer@users.noreply.github.com>
2025-05-06 12:00:50 +0530

    python chapter 2 prfactice set problem 1

---
+++ Chapter2/problem1.py
@@ -0,0 +1,4 @@
+a = 1
+b = 3
+c = a + b
+print("The sum of A and B:" , c) #the print function adds a single white space before the ending double quote
---
+++ Chapter2/problem2.py
@@ -0,0 +1,3 @@
+a = input("Enter no. 1: ")
+b = input("Enter no. 2: ")
+print()
